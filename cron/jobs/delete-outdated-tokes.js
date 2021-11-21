47d6fa0f39f0a1c4ac85a4958f112dda2e052494
Alexander Vu <alexander.vu.tuyet@gmail.com>
2021-11-21 23:31:28 +0100

    Update neo4j update

--- cron/jobs/delete-outdated-tokes.js
+++ cron/jobs/delete-outdated-tokes.js
@@ -1,24 +1,21 @@
-import neo4j from 'neo4j-driver'
+import {driver} from '../neo4j.js'
 import {config} from '../../config.js'
 import {logger} from '../logger.js'
 
+const session = driver.session()
+
 try {
-  const driver = neo4j.driver(
-    config.VL_NEO4J_DB,
-    neo4j.auth.basic(config.VL_NEO4J_AUTH_USER, config.VL_NEO4J_AUTH_PASSWORD))
-
-  const session = driver.session({defaultAccessMode: neo4j.session.WRITE})
-
-  await session
-    .run(`MATCH (t:Singtoken) WHERE t.created < DateTime() - duration({hours: ${config.VL_NEO4J_TOKEN_HOURS_EXPIRE}}) DETACH DELETE t RETURN COUNT(t) AS affected`)
-    .then(result => {
-      for (const record of result.records) {
-        logger.info({deleted: record.get('affected').toNumber()})
-      }
-    })
-    .catch(error => logger.error(error))
-    .then(() => session.close())
+  const affected_tokens = await session.writeTransaction(async tx => {
+    const result = await tx.run(
+      'MATCH (t:Singtoken) WHERE t.created < DateTime() - duration({hours: $token_expire}) DETACH DELETE t RETURN COUNT(t) AS affected',
+      { token_expire: config.VL_NEO4J_TOKEN_HOURS_EXPIRE }
+    )
+    return result.records.map(record => record.get('affected').toNumber())
+  })
+  logger.info({message: 'Delete outdated tokens', count:  affected_tokens[0]})
+} catch (executionError) {
+  logger.error(executionError)
+} finally {
+  await session.close()
   await driver.close()
-} catch (error) {
-  logger.error(error)
 }
